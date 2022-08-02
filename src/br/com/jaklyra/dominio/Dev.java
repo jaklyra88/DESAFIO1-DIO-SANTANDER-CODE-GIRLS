702662a721bf7ab3778471e67b664ebaac77576b
Jakelyne Lyra <jaklyra@gmail.com>
2022-08-02 14:00:33 -0300

    Conclusão das Classes com adição de regras de negócio - Desafio 1 - Santander Code Girls

--- src/br/com/jaklyra/dominio/Dev.java
+++ src/br/com/jaklyra/dominio/Dev.java
@@ -3,6 +3,7 @@ package br.com.jaklyra.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -23,16 +24,41 @@ public class Dev {
     //QUANDO CHAMAR O MÉTODO INSCREVER BOOTCAMP E PASSAR O BOOTCAMP, SERÁ EFETUADA A INSCRIÇÃO
     //TERÁ ACESSO AO CONTEUDO INSCRITO
     public void inscreverBootcamp(Bootcamp bootcamp){
+        //PEGANDO O SET DE CONTEUDOS INSCRITOS E ADICIONANDO DENTRO DELE TODOS OS CONTEUDOS DO BOOTCAMP
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
 
+        //ADICIONANDO O DEV NO BOOTCAMP
+        //PEGANDO O BOOTCAMP - PEGANDO O GETDEVS INCRITOS QUE CONTEM TODOS OS DEVS INSCRITOS
+        //ADICIONANDO O DEV
+        bootcamp.getDevsInscritos().add(this);
     }
 
     //A MEDIDA EM QUE PROGREDIMOS NO BOOTCAMP - TEREMOS CONTEUDOS CONCLUIDOS
+    //O MÉTODO FINDFIRST RETORNA UM OPTIONAL - PARA RESOLVER A QUESTÃO DE RETORNOS NULOS
     public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();//PEGANDO O PRIMEIRO CONTEUDO
 
+            //SE O CONTEUDO EXISTIR EM CONTEUDO
+            if(conteudo.ifPresent()){
+                //COLOCA O CONTEÚDO DENTRO DO SET DE CONTEUDOS CONCLUIDOS
+                this.conteudosConcluidos.add(conteudo.get());
+                //REMOVENDO DO SET DE CONTEUDOSINSCRITOS
+                this.conteudosInscritos.remove(conteudo.get());
+            }else{
+                System.err.println("Você não está matriculado em nenhum conteúdo!");
+            }
     }
 
     //AO CHAMAR - SABEREMOS QUANTO XP O DEV CONSEGUIU
-    public void calculartOTALXp(){
+    public double calcularTotalXp(){
+
+        //PEGANDO O SET DE CONTEUDOS CONCLUIDOS
+        //STREAMAPI PEGANDO CADA CONTEUDO DE DENTRO DO SET DE CONTEUDOS CONCLUIDOS
+        //E PEGANDO O XP DE CADA CONTEUDO E SOMANDO
+        return this.conteudosConcluidos
+                .stream()
+                .mapToDouble(Conteudo::calcularXp)
+                .sum();
 
     }
 
